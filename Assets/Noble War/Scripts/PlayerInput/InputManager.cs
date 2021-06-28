11536e72f9c319f50c951a875be488558a174e32
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2021-06-28 18:17:45 +0300

    +create basic input system

---
+++ Assets/Noble War/Scripts/PlayerInput/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace NobleWar.PlayerInput
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private InputData[] _inputDataArray;
+        private void Update()
+        {
+            for (int i = 0; i < _inputDataArray.Length; i++)
+            {
+                _inputDataArray[i].ProcessInput();
+            }
+        }
+    }
+}
